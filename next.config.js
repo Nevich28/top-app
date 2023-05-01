adf016464cc5baa363ad42bab31a0e4c277b1e60
Andrey <andrey.liubashchenko@gmail.com>
2023-05-02 00:58:49 +0300

    Rating

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,--legacy-peer-deps
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+	images: {
+		domains: ['courses-top.ru']
+	},
+	webpack(config, options) {
+		config.module.rules.push({
+			loader: '@svgr/webpack',
+			issuer: /\.[jt]sx?$/,
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [{ 
+						name: 'preset-default',
+						params: {
+							override: {
+								removeViewBox: false
+							}
+						}
+					}],
+				},
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
+
+		return config;
+	},
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+	return  <>
+				<Head>
+					<title>MyTop - наш лучший топ</title>
+					<link rel="preconnect" href="https://fonts.gstatic.com"/>
+					<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"/>
+					<link key={1} rel="icon" href="/favicon.ico" />
+				</Head>
+				<Component {...pageProps} />
+			</>;
 }
