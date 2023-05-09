6b1c342963ce8d420ede1f811701a018f15c69e1
Andrey <andrey.liubashchenko@gmail.com>
2023-05-10 01:15:17 +0300

    husky

--- src/components/hhData/HhData.tsx
+++ src/components/hhData/HhData.tsx
@@ -10,12 +10,12 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
 		<div className={styles.hh}>
 			<Card className={styles.count}>
 				<div className={styles.title}>Всего вакансий</div>
-				<div className={styles.countValue}>{count}</div>
+				<div className={styles.countvalue}>{count}</div>
 			</Card>
 			<Card className={styles.salary}>
 				<div>
 					<div className={styles.title}>Начальный</div>
-					<div className={styles.salaryValue}>{priceRu(juniorSalary)}</div>
+					<div className={styles.salaryvalue}>{priceRu(juniorSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled}/>
 						<RateIcon/>
@@ -24,7 +24,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
 				</div>
 				<div>
 					<div className={styles.title}>Средний</div>
-					<div className={styles.salaryValue}>{priceRu(middleSalary)}</div>
+					<div className={styles.salaryvalue}>{priceRu(middleSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled}/>
 						<RateIcon className={styles.filled}/>
@@ -33,7 +33,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
 				</div>
 				<div>
 					<div className={styles.title}>Профессионал</div>
-					<div className={styles.salaryValue}>{priceRu(seniorSalary)}</div>
+					<div className={styles.salaryvalue}>{priceRu(seniorSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled}/>
 						<RateIcon className={styles.filled}/>
